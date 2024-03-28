9a121749aa5c0abc609680b83d9615b51a3ee921
nathen321 <walidjalaly43@gmail.com>
2024-03-28 14:08:24 -0700

    test task 2

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creat a node
+ *
+ * @parent: parent node
+ * @value: vaue atributed to the node
+ *
+ * Return: Always binary_tree_t
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+	parent->left = new_node;
+	return (parent);
+}
