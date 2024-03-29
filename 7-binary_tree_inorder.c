f203fdd2c9f1c85fc624e74be9b18a394918267e
nathen321 <walidjalaly43@gmail.com>
2024-03-28 17:43:28 -0700

    exception

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - creat a node
+ *
+ * @tree: root node
+ * @func: function cal to print value of node
+ *
+ * Return: void
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
